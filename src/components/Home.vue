474d185330d18863971e4d26dc20d5833b3ca21b
AnwarHossainSR <mahedianwar@gmail.com>
2021-05-19 10:47:46 +0600

    radio button and check box

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,23 @@
     <div>
         
         <h1>Login</h1><br>
-    
-        <input type="text" name="email" v-model="email"><br>
-    
-        <input type="text" name="password" v-model="password"><br>
-    
-        <button v-on:click="getData">Submit</button>
+        <label for="java">Java</label>
+        <input type="checkbox" id="java" value="Java" v-model="technology">
+        <label for="php">Php</label>
+        <input type="checkbox" id="php" value="Php" v-model="technology">
+        <label for="python">Python</label>
+        <input type="checkbox" id="python" value="Python" v-model="technology"><br>
+
+        <h1>Who i am</h1>
+        <label for="developer">Developer</label>
+        <input type="radio" name="profession" value="Developer" id="developer" v-model="who">
+        <label for="student">Student</label>
+        <input type="radio" name="profession" value="Student" id="student" v-model="who">
+        <label for="business">Business man</label>
+        <input type="radio" name="profession" value="Businessman" id="business" v-model="who"><br><br>
+
+        <h3>Technologies : {{ technology }}</h3><br>
+         <h3>Who i am : {{ who }}</h3>
     
     </div>
 </template>
@@ -15,18 +26,10 @@
 <script>
 export default {
     name: 'Home',
-    props: {
-        name: String
-    },
     data() {
         return {
-            email:null,
-            password:null
-        }
-    },
-    methods: {
-        getData() {
-            alert(this.email +" "+this.password)
+            technology:[],
+            who:null
         }
     }
 }
